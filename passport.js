5e549ba2284a0916b41a31b07dc86495c812d0b1
Dewitt Buckingham <dewitt@zerodaedal.us>
2025-01-15 21:18:28 -0800

    refactor: Remove default for OAuth callback

--- passport.js
+++ passport.js
@@ -8,9 +8,7 @@ const googleStrategy = new Strategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:
-      process.env.GOOGLE_CALLBACK_URL ||
-      "http://localhost:3000/auth/google/callback",
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
   },
   verifyUser
 );
